84d275d9422f0b1551d39548b61657c51b3834d8
Magnarius <tishagly@gmail.com>
2025-07-27 12:57:07 +0800

    fix grid and profile popup

--- main.js
+++ main.js
@@ -39,4 +39,27 @@ $(function () {
       closeMenu();
     }
   });
+
+  // Profile submenu on mobile devices
+
+  const profileTrigger = document.querySelector('.profile-hover');
+  const profilePopup = document.querySelector('.profile-popup');
+
+  const isTouchDevice = matchMedia(
+    '(hover: none) and (pointer: coarse)'
+  ).matches;
+
+  if (isTouchDevice) {
+    profileTrigger.addEventListener('click', (e) => {
+      e.preventDefault();
+      e.stopPropagation();
+      profilePopup.style.display =
+        profilePopup.style.display === 'flex' ? 'none' : 'flex';
+    });
+
+    document.addEventListener('click', (e) => {
+      e.preventDefault();
+      profilePopup.style.display = 'none';
+    });
+  }
 });

c743e16ec7785b37c28ae4c0eb3b7361cc61c70c
Magnarius <tishagly@gmail.com>
2025-07-29 21:58:32 +1000

    fix filters for and add bottom menu, rm burger menu
    fix padding
    
    fix mobile header
    
    rm burger menu

--- main.js
+++ main.js
@@ -1,45 +1,4 @@
 $(function () {
-  var $burger = $('#burgerBtn');
-  var $menu = $('#mainMenu');
-  var $overlay = $('#mobileMenuOverlay');
-  var $closeBtn = $('#closeMobileMenu');
-  function openMenu() {
-    $menu.addClass('open').attr('aria-hidden', 'false');
-    $overlay.show();
-    $burger.addClass('open').attr('aria-expanded', 'true');
-  }
-  function closeMenu() {
-    $menu.removeClass('open').attr('aria-hidden', 'true');
-    $overlay.hide();
-    $burger.removeClass('open').attr('aria-expanded', 'false');
-    $('body').css('overflow', '');
-  }
-  function toggleMenu() {
-    if ($menu.hasClass('open')) {
-      closeMenu();
-    } else {
-      openMenu();
-    }
-  }
-  $burger.on('click keydown', function (e) {
-    if (
-      e.type === 'click' ||
-      (e.type === 'keydown' && (e.key === 'Enter' || e.key === ' '))
-    ) {
-      toggleMenu();
-    }
-  });
-  $overlay.on('click', closeMenu);
-  if ($closeBtn && $closeBtn.length) {
-    $closeBtn.on('click', closeMenu);
-  }
-  // Close on ESC
-  $(document).on('keydown', function (e) {
-    if ($menu.hasClass('open') && e.key === 'Escape') {
-      closeMenu();
-    }
-  });
-
   // Profile submenu on mobile devices
 
   const profileTrigger = document.querySelector('.profile-hover');
